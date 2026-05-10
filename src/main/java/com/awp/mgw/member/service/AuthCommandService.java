3305454f6f383397e0c80876495d6f66d84bfb9d
yerin <chayerin123@gmail.com>
2026-05-11 01:01:24 +0900

    chore: 누락된 import문 추가

--- src/main/java/com/awp/mgw/member/service/AuthCommandService.java
+++ src/main/java/com/awp/mgw/member/service/AuthCommandService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.awp.mgw.global.security.jwt.JwtTokenProvider;
 
 @Service
 @RequiredArgsConstructor
