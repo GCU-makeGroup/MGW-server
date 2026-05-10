341c569d79322ca41c85c92f7f87ca19de733310
yerin <chayerin123@gmail.com>
2026-05-11 02:01:22 +0900

    feat: 세션 관리 STATELESS 설정 추가

--- src/main/java/com/awp/mgw/common/config/SecurityConfig.java
+++ src/main/java/com/awp/mgw/common/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig {
@@ -19,6 +20,9 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
           .csrf(csrf -> csrf.disable())
+          .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          )
           .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
           )
