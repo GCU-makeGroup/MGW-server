6bc75823125d61b14656692a720f8028f8ea1d6b
yerin <chayerin123@gmail.com>
2026-05-12 00:25:27 +0900

    feature: 외부 호출을 위한 schedule controller 생성

---
+++ src/main/java/com/awp/mgw/schedule/controller/ScheduleController.java
@@ -0,0 +1,33 @@
+package com.awp.mgw.schedule.controller;
+
+import com.awp.mgw.schedule.controller.dto.response.ScheduleDateResponse;
+import com.awp.mgw.schedule.usecase.query.GetMonthlyScheduleUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.YearMonth;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/schedules")
+public class ScheduleController {
+
+  private final GetMonthlyScheduleUseCase getMonthlyScheduleUseCase;
+
+  @GetMapping
+  public List<ScheduleDateResponse> getMonthlySchedules(
+        @AuthenticationPrincipal Long memberId,
+        @RequestParam int year,
+        @RequestParam int month
+  ) {
+    return getMonthlyScheduleUseCase.getMonthlySchedules(
+          memberId,
+          YearMonth.of(year, month)
+    );
+  }
+}
