270591d3f13d6ff70a464329cb58b12778db1f8d
yerin <chayerin123@gmail.com>
2026-05-12 14:49:02 +0900

    feature: 연/월 검증 로직 추가

--- src/main/java/com/awp/mgw/mypage/controller/MyPageController.java
+++ src/main/java/com/awp/mgw/mypage/controller/MyPageController.java
@@ -7,9 +7,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.YearMonth;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class MyPageController {
@@ -20,6 +24,12 @@ public class MyPageController {
   public MyPageMainResponse getMyPageMain(
         @AuthenticationPrincipal Long memberId,
         @RequestParam int year,
+
+        @Min(value = 2000, message = "연도는 2000년 이상이어야 합니다.")
+        @RequestParam int year,
+
+        @Min(value = 1, message = "월은 1 이상이어야 합니다.")
+        @Max(value = 12, message = "월은 12 이하여야 합니다.")
         @RequestParam int month
   ) {
     return getMyPageMainUseCase.getMyPageMain(
